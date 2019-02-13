f5e39175fd14a2d83df36b325751742d44b793eb
Andrey Arias <andrey.arias@cireson.com>
2019-02-12 16:27:24 -0800

    Add project files

--- Build/android/02_07_2014_1/src/scanner/src/main/java/com/cireson/scanner/splash/SplashScreen.java
+++ Build/android/02_07_2014_1/src/scanner/src/main/java/com/cireson/scanner/splash/SplashScreen.java
@@ -1,3 +1,22 @@
+
+//////////////////////////////////////////////////////////////////////////////
+//This file is part of Cireson Barcode Scanner. 
+//
+//Cireson Barcode Scanner is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//Cireson Barcode Scanner is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with Cireson Barcode Scanner.  If not, see<https://www.gnu.org/licenses/>.
+/////////////////////////////////////////////////////////////////////////////
+
+
 package com.cireson.scanner.splash;
 
 import android.app.Activity;
@@ -8,9 +27,6 @@ import android.content.Intent;
 import com.cireson.scanner.view.LoginActivity;
 import com.cireson.scanner.R;
 
-/**
- * Created by seattleapplab on 1/31/14.
- */
 public class SplashScreen extends Activity {
 
     private int SPLASH_TIMEOUT = 3000;
